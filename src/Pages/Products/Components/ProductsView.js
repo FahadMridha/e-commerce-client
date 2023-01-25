7d65366a610369cefa88a7d01a5b7cd40e8b0cc4
fahad Mridha <sohelcse16@gmail.com>
2023-01-26 03:24:42 +0600

    checkout page added

--- src/Pages/Products/Components/ProductsView.js
+++ src/Pages/Products/Components/ProductsView.js
@@ -1,8 +1,13 @@
 import { Card } from "flowbite-react";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProductsView = ({ product }) => {
   const { _id, name, price, details, picture } = product;
+  const navigate = useNavigate();
+  const handleDetail = () => {
+    navigate("/details", { state: product });
+  };
 
   return (
     <div>
@@ -17,10 +22,16 @@ const ProductsView = ({ product }) => {
           </p>
           <p className="font-medium">{price}</p>
           <div className="mt-4 flex space-x-3 lg:mt-6">
-            <button className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-4 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button
+              onClick={handleDetail}
+              className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-4 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
               Add to Cart
             </button>
-            <button className="inline-flex items-center rounded-lg border border-gray-300 bg-white py-2 px-4 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+            <button
+              onClick={handleDetail}
+              className="inline-flex items-center rounded-lg border border-gray-300 bg-white py-2 px-4 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+            >
               View Details
             </button>
           </div>
--- src/Routes/router.js
+++ src/Routes/router.js
@@ -3,7 +3,10 @@ import MainLoyout from "../Layouts/MainLayout/MainLoyout";
 import Login from "../Pages/Authentication/Login/Login";
 import Home from "../Pages/HomePage/Home/Home";
 import MyCart from "../Pages/myCart/MyCart";
+import CheckOut from "../Pages/Products/Components/CheckOut";
+import ViewDetails from "../Pages/Products/Components/ViewDetails";
 import Products from "../Pages/Products/Products";
+import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +20,26 @@ const router = createBrowserRouter([
       {
         path: "/products",
         element: <Products />,
+      },
+      {
+        path: "/details",
+        element: <ViewDetails />,
+      },
+      {
+        path: "/order",
+        element: <PrivateRoute>{/* <Order></Order> */}</PrivateRoute>,
+      },
+      {
+        path: "/checkout/:id",
+        element: (
+          <PrivateRoute>
+            <CheckOut></CheckOut>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(
+            `https://e-commerce-task-server.vercel.app/product/${params.id}`
+          ),
       },
       {
         path: "/mycart",
